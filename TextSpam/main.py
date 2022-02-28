23a23bc584aceb8e1d26edb7a55d21c673a95698
Elijah <victoryelijahberry@gmail.com>
2022-02-28 05:16:40 -0500

    Added 'initiate' function'

--- TextSpam/main.py
+++ TextSpam/main.py
@@ -8,10 +8,11 @@ from time import sleep
 # Used to quickly set up the array to iterate through
 # instead of having a long list to copy-paste
 # Will style later
-def initiate():
-        base_macro = [keyboard.write('hi'), sleep(1), keyboard.send('enter'), sleep(1), keyboard.write('How are you?'),
-                      sleep(1), keyboard.send('backspace'), sleep(1)]
-        return base_macro
+def initiate(base_macro):
+
+         base_macro = [keyboard.write('hi'), sleep(1), keyboard.send('enter'), sleep(1), keyboard.write('How are you?'),
+                       sleep(1)]
+         return base_macro
 
 
 
@@ -30,7 +31,7 @@ not meant to be used for any trolling purposes""")
         0 for no: """)
     # Making sure that the answer is an INT and not a letter
     agreement_answer = int(agreement_answer)
-    #This is incase someone puts a letter inside of it.
+    #This is in case someone puts a letter inside the prompt.
     if agreement_answer == 0 or 1:
         if agreement_answer == 1:
             # 0 or 1 are the only acceptable answers
@@ -49,26 +50,29 @@ not meant to be used for any trolling purposes""")
     else:
         print('no you do not.')
         exit()
-def prompt():
-    number = input('Enter a number')
-    number = int(number)
-    textSpam(number)
-def text_spam(time):
 
-    base = initiate()
+# def prompt():
+#     number = input('Enter a number')
+#     number = int(number)
+#     textSpam(number)
+def text_spam(time):
+    i = 0
+    base = []
 
     for i in range(time):
-        if i <= range(base):
-            for i in range:
-                if i == 0:
-                    print('Program will begin in five seconds. please open notepad.')
-                    sleep(5.00)
-                else:
-                    base[i]
-                    quit()
-        else:
-            print("that is not a number")
-            quit()
+        if i <= 0:
+
+            print('5 seconds.')
+            sleep(5.00)
+            initiate(base)
+
+
+
+
+
+
+
+
 
 
 agreement()
