67b05d521c50a35b4cdc0db05dd872ec591e68fc
Elijah <victoryelijahberry@gmail.com>
2022-02-28 04:15:09 -0500

    Improved/Cleaner Code. Added letter check currently has bugs

--- TextSpam/main.py
+++ TextSpam/main.py
@@ -1,42 +1,75 @@
+from typing import Any
+
 import keyboard
 import mouse
 from time import sleep
 
 
-# This program is for joke purposes and is not to be used to
+# Used to quickly set up the array to iterate through
+# instead of having a long list to copy-paste
+# Will style later
+def initiate():
+        base_macro = [keyboard.write('hi'), sleep(1), keyboard.send('enter'), sleep(1), keyboard.write('How are you?'),
+                      sleep(1), keyboard.send('backspace'), sleep(1)]
+        return base_macro
 
-#
-def textSpam(time):
-    i = 0
-    if isinstance(time, int):
-        for i in range(time):
-            if i < 1:
-                print('Program will begin in five seconds. please open program.')
-                sleep(5.00)
-            else:
-                keyboard.write('hi')
-                # sleep(3)
-                # keyboard.send('backspace')
-                # keyboard.send('backspace')
-                # sleep(2)
-    else:
-        print('please enter a number.')
-    keyboard.add_hotkey('left windows', quit(textSpam()))
+
+
+# What starts the entire program. If you agree you
+# get prompted and begin the text_spam.
 def agreement():
-    print("""This program is intended for joke purposes and 
-    is not meant to be used for any trolling purposes""")
-    agreementanswer = input("""Do you understand?  
-0 for yes 1 for no: """)
+    # Don't use this to be harmful.
+    print(
+        """This program is intended for joke purposes and is 
+not meant to be used for any trolling purposes""")
 
-    if agreementanswer == 0 or 1:
-        repeat = input('How many times would you like this repeated?: ')
-        repeat = int(repeat)
+    # Actual program starts here.
+    agreement_answer = input(
+        """Do you understand?  
+        1 for yes 
+        0 for no: """)
+    # Making sure that the answer is an INT and not a letter
+    agreement_answer = int(agreement_answer)
+    #This is incase someone puts a letter inside of it.
+    if agreement_answer == 0 or 1:
+        if agreement_answer == 1:
+            # 0 or 1 are the only acceptable answers
+            repeat = input('How many times would you like this repeated? (2-10 recommend): ')
+            repeat = int(repeat)
+            # runs through the main program.
+            text_spam(repeat)
+        # For now the programs will just end.
+        elif agreement_answer == 0:
+            print('Sorry.')
+            exit()
 
-        textSpam(repeat)
+        else:
+            print('no you do not.')
+            exit()
     else:
-        print('please follow instructions.')
+        print('no you do not.')
+        exit()
+def prompt():
+    number = input('Enter a number')
+    number = int(number)
+    textSpam(number)
+def text_spam(time):
+
+    base = initiate()
+
+    for i in range(time):
+        if i <= range(base):
+            for i in range:
+                if i == 0:
+                    print('Program will begin in five seconds. please open notepad.')
+                    sleep(5.00)
+                else:
+                    base[i]
+                    quit()
+        else:
+            print("that is not a number")
+            quit()
 
 
-
-
-
+agreement()
+# textSpam(3)
