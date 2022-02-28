790c4e8550912c5c657a620dbc9aab5aab04a735
Elijah <victoryelijahberry@gmail.com>
2022-02-28 05:49:55 -0500

    Cleaned Up

--- TextSpam/main.py
+++ TextSpam/main.py
@@ -5,46 +5,40 @@ import mouse
 from time import sleep
 
 
-# Used to quickly set up the array to iterate through
-# instead of having a long list to copy-paste
-# Will style later
-def initiate(base_macro):
 
-         base_macro = [keyboard.write('hi'), sleep(1), keyboard.send('enter'), sleep(1), keyboard.write('How are you?'),
-                       sleep(1), keyboard.send('enter')]
-         return base_macro
-# What starts the entire program. If you agree you
-# get prompted and begin the text_spam.
-def agreement():
-    # Don't use this to be harmful.
+
+def start_up():
     print(
         """This program is intended for joke purposes and is 
 not meant to be used for any trolling purposes""")
-
-    # Actual program starts here.
     agreement_answer = input(
         """Do you understand?  
         1 for yes 
         0 for no: """)
-    # Making sure that the answer is an INT and not a letter
     agreement_answer = int(agreement_answer)
-    #This is in case someone puts a letter inside the prompt.
 
+
+    # 0 or 1 are the only acceptable answers. Then runs it through the main function
     if agreement_answer == 1:
-        # 0 or 1 are the only acceptable answers
         repeat = input('How many times would you like this repeated? (2-10 recommend): ')
         repeat = int(repeat)
-        # runs through the main program.
         text_spam(repeat)
+
     # For now the programs will just end.
     elif agreement_answer == 0:
         print('Sorry.')
         exit()
     # This only happens if a letter or 3< is put in
     else:
-        print('no you do not.')
+        print('Sorry.')
         exit()
-
+#initiates the hardcoded macro and returns an array.
+def initiate_macro(base_macro):
+    base_macro = [keyboard.write('hi'),
+    sleep(1), keyboard.send('enter'),
+    sleep(1), keyboard.write('How are you?'),
+    sleep(1), keyboard.send('enter')]
+    return base_macro
 
 def text_spam(time):
     # Set variable to assign later.
@@ -55,9 +49,8 @@ def text_spam(time):
             print('5 seconds.')
             sleep(5.00)
         else:
-            initiate(base)
+            initiate_macro(base)
             i += 1
 
+start_up()
 
-agreement()
-# textSpam(3)
